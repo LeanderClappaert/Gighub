2e932d2332b3f1efbcffcd1f03d707f559d17ae2
Leander Clappaert <Leander.Clappaert@lansweeper.com>
2018-03-23 16:54:16 +0100

    Implement saving a gig.

--- Gighub/Gighub/Gighub/Controllers/GigsController.cs
+++ Gighub/Gighub/Gighub/Controllers/GigsController.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using Gighub.Models;
 using Gighub.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace Gighub.Controllers
 {
@@ -14,6 +16,7 @@ namespace Gighub.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -22,6 +25,39 @@ namespace Gighub.Controllers
             };
 
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            /*
+                This used to be 2 calls to the DB to get the artist and genre for the currently logged in user. 
+                But by changing the Domain Model (adding FK's GenreId and ArtistId) these calls are unnecessary.
+                --> When loading the view, everything is loaded using 1 call to the DB.
+
+                var artistId = User.Identity.GetUserId();
+                var artist = _context.Users.Single(u => u.Id == artistId);
+                var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            */
+
+            var gig = new Gig
+            {
+                /*
+                    Artist = artist,
+                    Genre = genre,
+                 */
+
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse($"{viewModel.Date} {viewModel.Time}"),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home"); //temporary send the user to the homepage.
         }
     }
 }
