b3373192f1134db6d7ee0932fc860f32d918bb7e
Leander Clappaert <Leander.Clappaert@lansweeper.com>
2018-03-26 18:41:29 +0200

    Implementing the API

---
+++ Gighub/Gighub/Gighub/Controllers/AttendancesController.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Web.Http;
+using Gighub.DTOs;
+using Gighub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace Gighub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+            var exists = _context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId);
+
+            if (exists)
+            {
+                return BadRequest("The attendance already exists.");
+            }
+
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ Gighub/Gighub/Gighub/DTOs/AttendanceDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Gighub.DTOs
+{
+    public class AttendanceDto
+    {
+        public int GigId { get; set; }
+    }
+}
